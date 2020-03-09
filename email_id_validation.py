451b9e104f920b895a64409b55abf3e9850fcd7d
Leo Johnson M <57273037+leojohnsonm@users.noreply.github.com>
2020-03-09 10:34:07 +0530

    Update email_id_validation.py

--- email_id_validation.py
+++ email_id_validation.py
@@ -1,7 +1,10 @@
 #this code fetches the email only 
+#logic - [a-z] - must start with a to z
+#\w - used for may be the mail end with digit 
+# + - used to add all the patterns 
 import re
 a = "123johns123@gmail.com123"
-x = re.findall("[a-z]+\d+@gmail.com",a)
+x = re.findall("[a-z]+\w+@gmail.com",a)
 print(x)
 exit()
 
@@ -9,7 +12,7 @@ exit()
 #this code fetches both mail and  mobile no 
 import re
 a = "johns123@gmail.comleo123@yahoo.com770895585396599527749944337977leo123@hotmail.com"
-x = re.findall("[a-z]+\d+@gmail.com|[a-z]+\d+@yahoo.com|[a-z]+\d+@outlook.com|[a-z]+\d+@hotmail.com|[6-9]\d{9}",a)
+x = re.findall("[a-z]+\w+@gmail.com|[a-z]+\w+@yahoo.com|[a-z]+\w+@outlook.com|[a-z]+\w+@hotmail.com|[6-9]\d{9}",a)
 print(x)
 exit()
 
