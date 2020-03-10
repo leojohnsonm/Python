f41db804c5de13670b64807a1159b5fcc418d650
Leo Johnson M <57273037+leojohnsonm@users.noreply.github.com>
2020-03-10 13:42:33 +0530

    Update mobile_no_validation_india.py
    Mobile no validation for mobile and  landline no's in india

--- mobile_no_validation_india.py
+++ mobile_no_validation_india.py
@@ -1,4 +1,19 @@
 import re
-a = input("enter the 10 digit mobile no: ")
-x = re.findall("[6-9]\d{9}", a)
-print(f"the entered no {x} is right")
+a = "044-2251529044-17412580444-369258"
+x = re.findall("[0]+\d{3}-\d{6}|[0]+\d{2}-\d{7}",a)
+print(x)
+exit()
+
+
+import re
+b = "7708955853979019749999443379779442273729"
+y = re.findall("[6-9]\d{9}",b)
+print(y)
+exit()
+
+
+import re
+b = "044-1472589044-3692581"
+y = re.findall("[044]+-\d{7}",b)
+print(y)
+exit()
