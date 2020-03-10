95053b4dbf3d29cdb35239f13f3da1399ce4a534
Leo Johnson M <57273037+leojohnsonm@users.noreply.github.com>
2020-03-10 13:51:56 +0530

    Add files via upload

---
+++ Mob no vld_File Handling.py
@@ -0,0 +1,9 @@
+import re
+import os #file handling n data fetching
+f = open("mail.txt","r") #("file_name","mode")
+pat = f.read() #data's in file assigned to "pat"
+x = re.findall("[a-z]+\w+@gmail.com|[a-z]+\w+@yahoo.com|[a-z]+\w+@hotmail.com|[a-z]+\w+@outlook.com",pat)
+y = re.findall("[6-9]\d{9}",pat)
+print(x)
+print(y)
+
