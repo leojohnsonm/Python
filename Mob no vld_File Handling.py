95cef2e09f3521e38a90397331263fdfdc3cd103
Leo Johnson M <57273037+leojohnsonm@users.noreply.github.com>
2020-03-11 09:36:46 +0530

    Update Mob no vld_File Handling.py

--- Mob no vld_File Handling.py
+++ Mob no vld_File Handling.py
@@ -1,7 +1,7 @@
 import re
-import os #file handling n data fetching
-f = open("mail.txt","r") #("file_name","mode")
-pat = f.read() #data's in file assigned to "pat"
+import os                 #file handling n data fetching
+f = open("mail.txt","r")  #("file_name","mode")
+pat = f.read()            #data's in file assigned to "pat"
 x = re.findall("[a-z]+\w+@gmail.com|[a-z]+\w+@yahoo.com|[a-z]+\w+@hotmail.com|[a-z]+\w+@outlook.com",pat)
 y = re.findall("[6-9]\d{9}",pat)
 print(x)
