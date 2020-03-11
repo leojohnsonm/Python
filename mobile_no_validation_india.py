776162d9db6c91b2336a4ed49b98b4cb0da08617
Leo Johnson M <57273037+leojohnsonm@users.noreply.github.com>
2020-03-11 12:51:22 +0530

    Update mobile_no_validation_india.py

--- mobile_no_validation_india.py
+++ mobile_no_validation_india.py
@@ -1,10 +1,10 @@
-import re  #starts woth                     
+import re  #starts with zero then the first digit must start with 6-9                     
 a = "0979019749917708955853919944337977"
 x = re.findall("^0+[6-9]\d{9}|[6-9]\d{9}",a)
 print(x)
 
 
-import re
+import re #starts with 91 then the first digit must start with 6-9
 a = "919790197499 917708955853 917239234673 919442273729 919659952774"
 x = re.findall("[91]([6-9]\d{9})",a)
 print(x)
